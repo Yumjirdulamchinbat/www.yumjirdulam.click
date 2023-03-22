af1133b6b977eb255b739ba74ffb5be14ca7e75b
Yumjirdulam Chinbat <yumjirdulamchinbat@Yumjirdulams-MacBook-Pro.local>
2023-03-22 00:06:54 -0500

    UI is changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -73,7 +73,7 @@ const handleChange = (event) => {
           {data.name} {data.email} {data.phone} {data.message}   
         </p> */}
 
-        <img class="blogImage" src="../../../blog.png" alt="Image" onClick={() => window.open(pdf)} />
+        <img id="blogImage" src="../../../blog.png" alt="Image" onClick={() => window.open(pdf)} />
 
 
       </form>
